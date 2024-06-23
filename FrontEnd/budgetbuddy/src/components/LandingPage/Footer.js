ba8cb890808111463851a626668eec19d8e92a16
matt7908 <mbuitrago00@mylangara.ca>
2024-06-23 10:44:07 -0700

    updated components of landing page
    updated the components, divided the landing page in different components for better organization

---
+++ FrontEnd/budgetbuddy/src/components/LandingPage/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer bg-light text-center">
+      <div className="container">
+        <p>© 2024 MewTwo | BudgetBuddy. All Rights Reserved</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
