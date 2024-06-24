58db96ce6dc82fa9c000e309bfd6f50364d05789
shunsaku-sugita <saku.dot@gmail.com>
2024-06-24 09:13:23 -0700

    add goal component of dashboard

--- FrontEnd/budgetbuddy/src/pages/DashboardPage.js
+++ FrontEnd/budgetbuddy/src/pages/DashboardPage.js
@@ -1,7 +1,36 @@
 import React from "react";
+import { Goals } from "../components/Dashboard/Goals";
 
-const DashboardPage = () => {
-  return <h1>DashboardPage TEST</h1>;
+export const DashboardPage = () => {
+  const jsonData = [
+    {
+      title: "Emergency Fund",
+      goal: 5000,
+      savings: 3200,
+    },
+    {
+      title: "Vacation",
+      goal: 2000,
+      savings: 1500,
+    },
+    {
+      title: "New Car",
+      goal: 15000,
+      savings: 4500,
+    },
+  ];
+
+  return (
+    <>
+      <h1>DashboardPage TEST</h1>
+      {jsonData.map((goal, index) => (
+        <Goals
+          key={index}
+          description={goal.title}
+          goal={goal.goal}
+          savings={goal.savings}
+        />
+      ))}
+    </>
+  );
 };
-
-export default DashboardPage;
