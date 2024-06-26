0492f1c72e74a71b153d414a76386ee2291b8ea4
shunsaku-sugita <saku.dot@gmail.com>
2024-06-25 17:07:20 -0700

    feat: add DashboardParts folder and GoalBarChart component of dashboard

--- FrontEnd/budgetbuddy/src/pages/DashboardPage.js
+++ FrontEnd/budgetbuddy/src/pages/DashboardPage.js
@@ -2,35 +2,9 @@ import React from "react";
 import { Goals } from "../components/Dashboard/Goals";
 
 export const DashboardPage = () => {
-  const jsonData = [
-    {
-      title: "Emergency Fund",
-      goal: 5000,
-      savings: 3200,
-    },
-    {
-      title: "Vacation",
-      goal: 2000,
-      savings: 1500,
-    },
-    {
-      title: "New Car",
-      goal: 15000,
-      savings: 4500,
-    },
-  ];
-
   return (
     <>
-      <h1>DashboardPage TEST</h1>
-      {jsonData.map((goal, index) => (
-        <Goals
-          key={index}
-          description={goal.title}
-          goal={goal.goal}
-          savings={goal.savings}
-        />
-      ))}
+      <Goals />
     </>
   );
 };
