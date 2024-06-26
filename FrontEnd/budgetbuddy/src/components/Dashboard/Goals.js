86c572658ccb1de67f2a66422d8f287dc0695967
shunsaku-sugita <saku.dot@gmail.com>
2024-06-25 17:07:20 -0700

    feat: add DashboardParts folder and GoalBarChart component of dashboard

--- FrontEnd/budgetbuddy/src/components/Dashboard/Goals.js
+++ FrontEnd/budgetbuddy/src/components/Dashboard/Goals.js
@@ -1,66 +1,37 @@
 import React from "react";
 import ReactApexChart from "react-apexcharts";
+import { GoalBarChart } from "../DashboardParts/GoalBarChart";
 
-export const Goals = ({ description, savings, goal }) => {
-  const percentage = (savings / goal) * 100;
-
-  const options = {
-    chart: {
-      sparkline: {
-        enabled: true,
-      },
-    },
-    plotOptions: {
-      bar: {
-        horizontal: true,
-        barHeight: "100%",
-        colors: {
-          backgroundBarColors: ["#CBE6FF"],
-          backgroundBarOpacity: 1,
-        },
-      },
-    },
-    // dataLabels: {
-    //   enabled: true,
-    //   formatter: function (val) {
-    //     return `${val}%`;
-    //   },
-    //   offsetX: 0,
-    //   style: {
-    //     colors: ["#CBE6FF"],
-    //   },
-    // },
-    xaxis: {
-      categories: ["Savings"],
-      max: 100,
-    },
-    fill: {
-      colors: ["#001E30"],
-    },
-  };
-
-  const series = [
+export const Goals = () => {
+  const jsonData = [
     {
-      name: "Savings",
-      data: [percentage],
+      title: "Buying a Toyota GR86",
+      goal: 3000,
+      savings: 2700,
+    },
+    {
+      title: "Buying a Toyota GR86",
+      goal: 3000,
+      savings: 2700,
+    },
+    {
+      title: "Buying a Toyota GR86",
+      goal: 3000,
+      savings: 2700,
     },
   ];
 
   return (
-    <div id="chart" style={{ margin: "0 20px 0 20px" }}>
-      <div style={{ textAlign: "left", marginTop: "10px", fontSize: "16px" }}>
-        {description} ({percentage}%)
-      </div>
-      <div>
-        $ {savings} / $ {goal}
-      </div>
-      <ReactApexChart
-        options={options}
-        series={series}
-        type="bar"
-        height={20}
-        borderRadius={50}
-      />
-    </div>
+    <>
+      <h3>Goals</h3>
+      {jsonData.map((goal, index) => (
+        <GoalBarChart
+          key={index}
+          description={goal.title}
+          goal={goal.goal}
+          savings={goal.savings}
+        />
+      ))}
+    </>
   );
 };
