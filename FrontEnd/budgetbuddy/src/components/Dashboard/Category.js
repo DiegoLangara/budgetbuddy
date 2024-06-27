18372e5c7bd7dedf0648c0f4bebd3a6ed84ec14b
shunsaku-sugita <saku.dot@gmail.com>
2024-06-27 16:37:15 -0700

    feat: add Category component and add react-table-library

---
+++ FrontEnd/budgetbuddy/src/components/Dashboard/Category.js
@@ -0,0 +1,58 @@
+import { CompactTable } from "@table-library/react-table-library/compact";
+
+export const Category = () => {
+  const nodes = [
+    {
+      category: "Auto",
+      budget: 200.0,
+      actual: 200.0,
+      difference: 0.0,
+    },
+    {
+      category: "Entertainment",
+      budget: 75.0,
+      actual: 32.0,
+      difference: 43.0,
+    },
+    {
+      category: "Food",
+      budget: 100.0,
+      actual: 200.0,
+      difference: 100.0,
+    },
+    {
+      category: "Home",
+      budget: 200.0,
+      actual: 200.0,
+      difference: 0.0,
+    },
+    {
+      category: "Medical",
+      budget: 75.0,
+      actual: 150.0,
+      difference: 75.0,
+    },
+    {
+      category: "Home",
+      budget: 300.0,
+      actual: 80.0,
+      difference: 20.0,
+    },
+  ];
+
+  const COLUMNS = [
+    { label: "Category", renderCell: (item) => item.category },
+    { label: "Budget", renderCell: (item) => item.budget },
+    { label: "Actual", renderCell: (item) => item.actual },
+    { label: "Difference", renderCell: (item) => item.difference },
+  ];
+
+  const data = { nodes };
+
+  return (
+    <>
+      <h3>Category</h3>
+      <CompactTable columns={COLUMNS} data={data} />
+    </>
+  );
+};
