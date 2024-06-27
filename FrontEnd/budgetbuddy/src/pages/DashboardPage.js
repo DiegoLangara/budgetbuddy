cbbdaee9eb4b77a4c91b186fd5923eeddd33b207
shunsaku-sugita <saku.dot@gmail.com>
2024-06-27 16:37:15 -0700

    feat: add Category component and add react-table-library

--- FrontEnd/budgetbuddy/src/pages/DashboardPage.js
+++ FrontEnd/budgetbuddy/src/pages/DashboardPage.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Goals } from "../components/Dashboard/Goals";
 import { Budget } from "../components/Dashboard/Budget";
 import { TrackExpenses } from "../components/Dashboard/TrackExpenses";
+import { Category } from "../components/Dashboard/Category";
 
 export const DashboardPage = () => {
   return (
     <>
       <Goals />
+      <Category />
       <Budget />
       <TrackExpenses />
     </>
