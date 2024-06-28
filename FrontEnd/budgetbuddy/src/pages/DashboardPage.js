d3d66692a3dbbbc49ade0dec902f1f5fc75cdfb1
shunsaku-sugita <saku.dot@gmail.com>
2024-06-27 17:57:10 -0700

    feat: add Income component of Dashboard and styling to dashboard

--- FrontEnd/budgetbuddy/src/pages/DashboardPage.js
+++ FrontEnd/budgetbuddy/src/pages/DashboardPage.js
@@ -3,14 +3,25 @@ import { Goals } from "../components/Dashboard/Goals";
 import { Budget } from "../components/Dashboard/Budget";
 import { TrackExpenses } from "../components/Dashboard/TrackExpenses";
 import { Category } from "../components/Dashboard/Category";
+import styled from "styled-components";
+import { Income } from "../components/Dashboard/Income";
 
 export const DashboardPage = () => {
   return (
-    <>
+    <StyledMainSection>
       <Goals />
       <Category />
       <Budget />
       <TrackExpenses />
-    </>
+      <Income />
+    </StyledMainSection>
   );
 };
+
+const StyledMainSection = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 1rem;
+  width: calc(100% - 4rem); /* Subtract the margins from the width */
+  margin: 2rem;
+`;
