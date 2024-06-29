daa45e094b7b6cac19369792dc1fd6e3961341ef
matt7908 <mbuitrago00@mylangara.ca>
2024-06-28 17:20:43 -0700

    updated UI on landing page
    css, and img

--- FrontEnd/budgetbuddy/src/components/LandingPage/Team.js
+++ FrontEnd/budgetbuddy/src/components/LandingPage/Team.js
@@ -1,6 +1,14 @@
 import React from "react";
 import '../../css/Team.css'
 import luis from '../../Assets/Luis.png'
+import Kyril from '../../Assets/Kyril.png'
+import Bill from '../../Assets/Bill.png'
+import darsh from '../../Assets/darsh.png'
+import diego from '../../Assets/diego.png'
+import yohei from '../../Assets/yohei.png'
+import shun from '../../Assets/shun.png'
+import mateo from '../../Assets/mateo.png'
+
 
 const Team = () => {
   return (
@@ -14,6 +22,55 @@ const Team = () => {
               <img src={luis} alt="Team Member" />
               <h3>Luis Baptista</h3>
               <p>Project Manager & UI/UX Designer</p>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="team-member">
+              <img src={Kyril} alt="Team Member" />
+              <h3>Kyril Evangelista</h3>
+              <p>Lead Designer - UI/UX </p>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="team-member">
+              <img src={Bill} alt="Team Member" />
+              <h3>Saharat AkaradechawutLuis Baptista</h3>
+              <p>UI/UX Designer </p>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="team-member">
+              <img src={darsh} alt="Team Member" />
+              <h3>Darshjot Sohi</h3>
+              <p>UI/UX Designer</p>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="team-member">
+              <img src={diego} alt="Team Member" />
+              <h3>Diego Almeida</h3>
+              <p>Lead Developer - Full Stack </p>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="team-member">
+              <img src={yohei} alt="Team Member" />
+              <h3>Yohei Tarutani</h3>
+              <p>Full Stack Developer </p>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="team-member">
+              <img src={shun} alt="Team Member" />
+              <h3>Shunsaku Sugita</h3>
+              <p>Full Stack Developer </p>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="team-member">
+              <img src={mateo} alt="Team Member" />
+              <h3>Mateo Buitrago</h3>
+              <p>Full Stack Developer </p>
             </div>
           </div>
         </div>
