87c48ea3d9ad602aaeca83ad49f8a400e7c0fffd
yohei-tarutani <152731552+yohei-tarutani@users.noreply.github.com>
2024-07-11 00:07:09 -0700

    feature: transaction table

--- FrontEnd/budgetbuddy/src/pages/ExpensesPage.js
+++ FrontEnd/budgetbuddy/src/pages/ExpensesPage.js
@@ -1,28 +1,24 @@
 import React, { useState, useEffect } from "react";
-import {
-  DataGrid,
-  GridToolbarContainer,
-  GridToolbarExport,
-} from "@mui/x-data-grid";
-import { useDemoData } from "@mui/x-data-grid-generator";
 import { Box } from "@mui/system";
 import { Field } from "../components/OnboardingParts/Field";
 import { Input } from "../components/OnboardingParts/Input";
 import { Button } from "react-bootstrap";
+import { ExpenseTable } from "../components/Expenses/ExpenseTable";
 
-function CustomToolbar() {
-  return (
-    <GridToolbarContainer>
-      <GridToolbarExport printOptions={{ disableToolbarButton: true }} />
-    </GridToolbarContainer>
-  );
-}
+const transactionCategories = [
+  "All types",
+  "goals",
+  "income",
+  "budgets",
+  "debts",
+];
 
 export const ExpensesPage = () => {
   const [startDate, setStartDate] = useState("");
   const [endDate, setEndDate] = useState("");
   const [appliedStartDate, setAppliedStartDate] = useState("");
   const [appliedEndDate, setAppliedEndDate] = useState("");
+  const [category, setCategory] = useState("All types");
 
   useEffect(() => {
     const today = new Date();
@@ -43,11 +39,9 @@ export const ExpensesPage = () => {
     setAppliedEndDate(endDate);
   };
 
-  const { data, loading } = useDemoData({
-    dataSet: "Commodity",
-    rowLength: 8,
-    maxColumns: 6,
-  });
+  const handleSelectChange = (e) => {
+    setCategory(e.target.value);
+  };
 
   return (
     <div
@@ -59,45 +53,60 @@ export const ExpensesPage = () => {
     >
       <div className="d-flex justify-content-between mb-4">
         <h2 className="mb-0">List of transactions</h2>
-        <button type="button" className="btn btn-secondary pl-4 pr-4">
+        <button type="button" className="btn btn-secondary pl-3 pr-3">
           {"+ "}Create
         </button>
       </div>
 
-      <Box display="flex" alignItems="stretch" gap={1}>
-        <Field label="Start date">
-          <Input
-            type="date"
-            value={startDate}
-            onChange={(e) => setStartDate(e.target.value)}
-          />
-        </Field>
-        <Field label="End date">
-          <Input
-            type="date"
-            value={endDate}
-            onChange={(e) => setEndDate(e.target.value)}
-          />
-        </Field>
-        <Field>
-          <Button onClick={handleApply} style={{ marginTop: "2rem" }}>
-            Apply
-          </Button>
-        </Field>
-      </Box>
+      <div className="d-flex filter-container">
+        <Box display="flex" alignItems="stretch" gap={1} className="mb-1">
+          <Field label="Start date" className="field">
+            <Input
+              type="date"
+              value={startDate}
+              onChange={(e) => setStartDate(e.target.value)}
+            />
+          </Field>
+          <Field label="End date" className="field">
+            <Input
+              type="date"
+              value={endDate}
+              onChange={(e) => setEndDate(e.target.value)}
+            />
+          </Field>
+          <Field className="field">
+            <Button
+              onClick={handleApply}
+              style={{ marginTop: "2rem", marginRight: "2rem" }}
+            >
+              Apply
+            </Button>
+          </Field>
+        </Box>
+        <Box className="type-wapper">
+          <Field label="Type" className="field">
+            <div>
+              <select
+                onChange={handleSelectChange}
+                className="form-select w-100 p-2 border border-secondary-subtle rounded rounded-2"
+                value={category}
+              >
+                {transactionCategories.map((option, index) => (
+                  <option key={index} value={option}>
+                    {option}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </Field>
+        </Box>
+      </div>
 
-      <div style={{ height: 600, width: "100%" }}>
-        <DataGrid
-          {...data}
-          loading={loading}
-          slots={{
-            toolbar: CustomToolbar,
-          }}
-          sx={{
-            "& .MuiDataGrid-columnHeaders": {
-              backgroundColor: "lightblue",
-            },
-          }}
+      <div style={{ height: "40vh", width: "100%" }}>
+        <ExpenseTable
+          startDate={appliedStartDate}
+          endDate={appliedEndDate}
+          category={category}
         />
       </div>
     </div>
